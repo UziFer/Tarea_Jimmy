e895f5133378c724eb37c950360cd315848f8497
UziFer <bermudez.rea.uzielfernando@gmail.com>
2023-11-22 13:20:04 -0600

    primer comitsito

---
+++ src/rsa_bueno/RSA_Bueno.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rsa_bueno;
+
+import Interfaz_Grafica.Menu;
+
+/**
+ *
+ * @author uzifer6789
+ */
+public class RSA_Bueno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
+    }
+    
+}
